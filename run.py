0f590acd3a3e2d870cee9bc11a9ba95962c5f590
nbutterly <butterlyn888@gmail.com>
2023-09-24 02:21:18 +0800

    chore(run): run example code for plantuml diagram

--- run.py
+++ run.py
@@ -1,52 +1,88 @@
 import logging
-import pandas as pd
-from template_project.helpers import (
-    SqlQuerier,
-    # getRichLogger,
-    # dictionary_deepest_key_value_pairs,
-    # flatten_dict,
-    # count_final_values_in_dict,
-    # validate_arguments,
+from src.template_project.helpers import render_plantuml_diagram
+from src.template_project.helpers import getRichLogger
+
+
+getRichLogger(
+    logging_level="INFO",
+    logger_name=__name__,
+    traceback_show_locals=True,
+    traceback_extra_lines=10,
+    traceback_suppressed_modules=(),
 )
 
+filepath: str = "test.json"
 
-WEMSDB_query_1 = """SELECT
-f.facility_id,
-fc.short_name facility,
-pc.short_name participant,
-cr.effective_date facility_creation_date
-FROM
-regn.facility_creation fc
-JOIN regn.facility f ON f.creation_id = fc.change_request_id
-JOIN regn.change_request cr ON cr.id = fc.change_request_id
-JOIN regn.participant p ON p.participant_id = fc.owner_id
-JOIN regn.participant_creation pc ON pc.change_request_id = p.creation_id
-ORDER BY facility_creation_date
-"""
+logging.info("Rendering plantuml diagram...")
+output_filepath: str = render_plantuml_diagram(
+    filepath=filepath,
+    diagram_format='png',
+    theme=None,
+    output_filepath=None,
+)
+logging.info("Rendering plantuml diagram... DONE")
 
-query_list: list[str] = [
-    WEMSDB_query_1,
-    WEMSDB_query_1,
-]
 
-# set the sql database to connect to
-sql_querier: SqlQuerier = SqlQuerier()
+print(output_filepath)
 
-# get sql data and store in a dataframe
-query: str
-pdf_list: list[pd.DataFrame] = []
-for query in query_list:
-    pdf_list += [
-        sql_querier.query_to_pandas_dataframe(
-            query=query,
-            db_name="WEMSDB"
-        )
-    ]
 
-logging.info("Printing dataframes...")
-print()
 
-pdf: pd.DataFrame
-for pdf in pdf_list:
-    print(pdf.head(10))
-    print()
+
+
+
+
+
+
+
+
+
+# from template_project.helpers import (
+#     SqlQuerier,
+#     # getRichLogger,
+#     # dictionary_deepest_key_value_pairs,
+#     # flatten_dict,
+#     # count_final_values_in_dict,
+#     # validate_arguments,
+# )
+
+
+# WEMSDB_query_1 = """SELECT
+# f.facility_id,
+# fc.short_name facility,
+# pc.short_name participant,
+# cr.effective_date facility_creation_date
+# FROM
+# regn.facility_creation fc
+# JOIN regn.facility f ON f.creation_id = fc.change_request_id
+# JOIN regn.change_request cr ON cr.id = fc.change_request_id
+# JOIN regn.participant p ON p.participant_id = fc.owner_id
+# JOIN regn.participant_creation pc ON pc.change_request_id = p.creation_id
+# ORDER BY facility_creation_date
+# """
+
+# query_list: list[str] = [
+#     WEMSDB_query_1,
+#     WEMSDB_query_1,
+# ]
+
+# # set the sql database to connect to
+# sql_querier: SqlQuerier = SqlQuerier()
+
+# # get sql data and store in a dataframe
+# query: str
+# pdf_list: list[pd.DataFrame] = []
+# for query in query_list:
+#     pdf_list += [
+#         sql_querier.query_to_pandas_dataframe(
+#             query=query,
+#             db_name="WEMSDB"
+#         )
+#     ]
+
+# logging.info("Printing dataframes...")
+# print()
+
+# pdf: pd.DataFrame
+# for pdf in pdf_list:
+#     print(pdf.head(10))
+#     print()
