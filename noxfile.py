6201dd07f96dfe71c81705f5e2496c9afab97d53
nbutterly <Nicholas.Butterly@aemo.com.au>
2023-09-29 01:39:22 +0800

    root(nox, dependencies): migrate to environment.yml
    - root(nox): update nox session 'test' to reference environment.yml
    - root(dependencies): migrated from requirements.txt. to environment.yml

--- noxfile.py
+++ noxfile.py
@@ -1,41 +1,157 @@
+# %%
+# IMPORTS
+# standard
+from pathlib import Path
+from typing import (
+    Generator,
+    Iterable,
+)
+# third party
 import nox
 
+# %%
+# CONFIGURATION
 
-@nox.session
+nox.options.sessions = []  # an empty list will display all sessions for command `nox`
+
+PYTHON_VERSIONS: str | list[str] = "3.10"
+
+
+# %%
+# FUNCTIONS
+
+def get_conda_environment_relative_filepaths(root_filepath: str = str(Path.cwd())) -> list[str]:
+    """Collect all conda environment*.yml filepaths relative to the root filepath of a project repository"""
+    conda_environment_paths: Generator[Path, None, None] = Path.glob(Path(root_filepath), "environment*.yml")
+    conda_environment_relative_filepaths: list[str] = [str(path.relative_to(root_filepath)) for path in conda_environment_paths]
+    return conda_environment_relative_filepaths
+
+
+def add_flags_to_cli_arugments(
+    cli_arugments: list[str],
+    flags: str | Iterable[str],
+) -> list[str]:
+    """
+    Adds flag to a list of command line interface (cli) arugments to prepare them to be passed to the command line.
+    e.g.:
+    >>> add_flag_to_flag_arumgnets(["environment.yml", "environment-dev.yml"], "-f")  # output: ["-f", "environment.yml", "-f", "environment-dev.yml"]
+    >>> subprocess("conda", "env", "update", *["-f", "environment.yml", "-f", "environment-dev.yml"], "--prune")  # update conda environment from environment.yml and environment-dev.yml
+    """
+    # resolve inputs
+    flags_list: list[str]
+    if isinstance(flags, Iterable):
+        flags_list = list(flags)  # convert to list if not already
+    if isinstance(flags, str):
+        flags_list = [flags] * len(cli_arugments)
+    # check inputs are valid
+    if len(flags_list) != len(cli_arugments):
+        raise ValueError(f"Length of flags ({len(flags_list)}) must match length of flag_arugments ({len(cli_arugments)})")
+    # add flags to flag_arugments
+    cli_arguments_with_flags: list[str] = []
+    for flag, argument in zip(flags_list, cli_arugments):
+        cli_arguments_with_flags.extend([flag, argument])
+    return cli_arguments_with_flags
+
+
+# %%
+# NOX SESSIONS
+
+# TODO: Make a 'setup' and 'run' sessions to setup and activate a conda environment, and run the program in that environment.
+# --> `conda env list --json` to ensure the new conda environment name doesn't clash, `venv_backend="none"`
+# --> https://nox.thea.codes/en/stable/cookbook.html
+
+@nox.session(python=PYTHON_VERSIONS)
 def cython_compile(session):
-    session.install("cython", "numpy")
+    session.run(
+        "pip",
+        "install",
+        "cython",
+        "numpy",
+    )
     session.run(
         "python",
-        "nox_scripts/cython_compile.py",
+        "cython_compile.py",
         "build_ext",
         "--inplace",
     )
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSIONS)
 def type_check(session):
-    session.install("mypy", "mypy-extensions")
-    session.run("mypy", "--ignore-missing-imports", ".")
+    session.run(
+        "pip",
+        "install",
+        "mypy",
+        "mypy-extensions",
+    )
+    session.run(
+        "mypy",
+        "--ignore-missing-imports",
+        ".",
+    )
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSIONS)
 def lint(session):
-    session.install("autopep8")
-    session.run("autopep8", "--in-place", "--recursive", ".")
+    session.run(
+        "pip",
+        "install",
+        "autopep8",
+    )
+    session.run(
+        "autopep8",
+        "--in-place",
+        "--recursive",
+        ".",
+    )
 
 
-@nox.session
+@nox.session(
+    venv_backend="conda",
+    python=PYTHON_VERSIONS,
+)
 def test(session):
-    session.install("pytest", "pytest-cov", "pytest-benchmark")
-    session.install("-r", "requirements.txt")
-    session.run("pytest", "tests")
+    # Collect all conda environment files
+    conda_environment_relative_filepaths: list[str] = get_conda_environment_relative_filepaths(root_filepath=str(Path.cwd()))
+    conda_install_environment_file_arugments: list[str] = add_flags_to_cli_arugments(
+        cli_arugments=conda_environment_relative_filepaths,
+        flags="-f"
+    )
+
+    session.run(
+        "conda",
+        "env",
+        "update",
+        *conda_install_environment_file_arugments,
+        "--prune",
+    )
+    session.run(
+        "pip",
+        "install",
+        "pytest",
+        "pytest-cov",
+        "pytest-benchmark",
+    )
+    session.run(
+        "pytest",
+        "tests",
+    )
 
 
 # TODO: add incremental typechecking, coverage, memory profiling, coverage, statistics, pyre configuration file
-@nox.session
+@nox.session(python=PYTHON_VERSIONS)
 def pyre(session):
-    session.install("pyre-check")
-    session.run("pyre", "--source-directory", ".", "check")
+    session.run(
+        "pip",
+        "install",
+        "pyre-check",
+    )
+    session.run(
+        "pyre",
+        "--source-directory",
+        ".",
+        "check",
+    )
 
 # # WIP not working yet. add automatic appending of @profile for functions
 # @nox.session
