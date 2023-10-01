81a5d98e63cadf63a0cae2e1bd818849c2874546
nbutterly <Nicholas.Butterly@aemo.com.au>
2023-10-01 14:00:24 +0800

    fix(cython): add pyMSVC for windows env setup

--- cython_compile.py
+++ cython_compile.py
@@ -1,8 +1,13 @@
 # `python cython_compile.py build_ext --inplace`
 
 from setuptools import setup
+import os
+import pyMSVC
 from Cython.Build import cythonize
 import numpy as np
+
+environment = pyMSVC.Environment()
+os.environ.update(environment)
 
 setup(
     ext_modules=cythonize(
